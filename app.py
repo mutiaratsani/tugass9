ed3657f351c765485f582e2b4e30be8441da0076
mutiaratsani <55040777+mutiaratsani@users.noreply.github.com>
2022-01-14 19:28:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
 model = keras.models.load_model('elephant_lion_class_model.h5')
 
-class_dict = {0: 'Burung Bangau', 1: 'Burung Merpati'}
+class_dict = {0: 'Burung Merpati', 1: 'Burung Bangau'}
 
 def predict_label(img_path):
     query = cv2.imread(img_path)
