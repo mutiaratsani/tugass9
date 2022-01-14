b6c757050f7219c3aee0016005c6452ed4bc5990
mutiaratsani <55040777+mutiaratsani@users.noreply.github.com>
2022-01-14 18:10:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, send_from_directory
 import keras
 import numpy as np
 import os
-import cv
+import cv2
 
 app = Flask(_name_)
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
